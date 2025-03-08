7dc47b524e302e68f03a8b4d86c91ac9f89bcde4
Avery Li <averyli1375@gmail.com>
2025-03-08 12:54:05 -0500

    ahhhhh

--- boop/urls.py
+++ boop/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for test_project project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+from dotenv import load_dotenv
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+load_dotenv()
+
+adminPath = os.getenv('ADMIN_PATH')
+if(adminPath == None):
+    adminPath = 'admin'
+
+# Admin Display
+admin.site.site_header = 'My Name'         
+admin.site.index_title = 'Portfolio'
+admin.site.site_title = 'Sandeep Shaw' 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include("app.urls")),
+    path(f'{adminPath}/', admin.site.urls),
 ]
